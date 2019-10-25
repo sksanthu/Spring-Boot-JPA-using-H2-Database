c664c838920fe88440892b3e999c27c1936df924
sksanthu <56837946+sksanthu@users.noreply.github.com>
2019-10-25 22:44:57 +0530

    Changes in main method

--- SpringH2UsingMultipleEntity/src/main/java/com/example/demo/SpringH2UsingMultipleEntityApplication.java
+++ SpringH2UsingMultipleEntity/src/main/java/com/example/demo/SpringH2UsingMultipleEntityApplication.java
@@ -1,13 +1,40 @@
 package com.example.demo;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringH2UsingMultipleEntityApplication {
-
+	//private static SpringH2UsingMultipleEntityApplication E;
 	public static void main(String[] args) {
 		SpringApplication.run(SpringH2UsingMultipleEntityApplication.class, args);
+		//E.saveEmployee();
 	}
-
-}
+		 @Autowired
+		    private EmployeeRepo employeeRepo;
+		    public void saveEmployee()
+		    {
+		        Employee employee = new Employee();
+		        employee.setEmpName("Santhosh");
+		        
+		        Employee_Address employeeAddress1 = new Employee_Address();
+		        employeeAddress1.setStreet("Street 1");
+		        employeeAddress1.setCity("City 1");
+		        employeeAddress1.setCountry("Country 1");
+		        employeeAddress1.setState("State 1");
+		        employeeAddress1.setEmployee(employee);
+		        
+		        
+		        Set<Employee_Address> employeeAddress = new HashSet<Employee_Address>();
+		        employeeAddress.add(employeeAddress1);
+		        
+		        employee.setEmployeeAddress(employeeAddress);
+		        
+		        employeeRepo.save(employee);
+		        System.out.println("Employee and Employee Address saved successfully!!");
+		    }
+		}
